2ab97c9f1512b156c334509037e89f7a0c0fa6bf
Stefano Ibarra <Ibarrste000@warren.k12.in.us>
2024-08-15 08:41:47 -0400

    Initial commit

---
+++ variables.py
@@ -0,0 +1,7 @@
+Number_of_apples = 20
+print("Number of apples:", Number_of_apples)
+Number_of_oranges = 15
+print("Number of oranges:", Number_of_oranges)
+print("Number of apples:", Number_of_apples)
+Number_of_oranges = 0
+print("Number of oranges:", Number_of_oranges)
